26ac682583b0f76f40e89f0c046dd09fec0c9e5e
francois141 <47925812+francois141@users.noreply.github.com>
2023-10-20 15:26:21 +0200

    Create pi.cpp

---
+++ MPI/pi.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#include <mpi.h>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    MPI_Init(&argc, &argv);
+
+    int rank;
+    int size;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    vector<int> a(10000);
+
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<double> dis(-1.0, 1.0);
+
+    generate(a.begin(), a.end(), [&dis, &gen]()->double {
+        double x = dis(gen);
+        double y = dis(gen);
+        return (x*x + y*y <= 1.0) ? 1.0 : 0.0;
+    });
+
+    double value = accumulate(a.begin(), a.end(), 0.0) / a.size();
+
+    double output;
+    MPI_Reduce(&value, &output, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if(rank == 0) {
+        cout << output << endl;
+    }
+
+    MPI_Finalize();
+    return 0;
+}
