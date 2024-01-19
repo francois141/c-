5f221ea25867fc8865144db140929c1a2be236d9
François Costa <frankost.costa@gmail.com>
2024-01-19 21:07:29 +0100

    Improve concept.cpp

--- concept.cpp
+++ concept.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 
 template <typename T>
-concept IntegralValue = std::is_integral<T>::value;
+concept IntegralValue = std::is_integral_v<T>;
+
+template <typename T>
+concept isNotPointer = not std::is_pointer_v<T>;
 
 template <IntegralValue T>
 T add(T a, T b) {
     return a + b;
 }
 
-template <typename T>
-requires std::is_signed<T>::value
+template <typename T> requires std::is_signed_v<T> and isNotPointer<T>
 T func(T param) {
     return param;
 }
 
+template<isNotPointer T>
+T mult(T a, T b) {
+    return a * b;
+}
 
 int main() {
     std::cout << "Sum: " << add(3, 4) << std::endl;
@@ -23,6 +29,9 @@ int main() {
 
     float val2 = 1.0;
     std::cout << "Sum: " << func(val2) << std::endl;
+
+    float val3 = 1.0;
+    std::cout << "Mult: " << mult(val3, val3) << std::endl;
 
     return 0;
 }
