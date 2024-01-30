41c156aeadd7f540c05bb208280ccd5773379abf
François Costa <frankost.costa@gmail.com>
2024-01-30 16:43:30 +0100

    Add mpi_compare_and_swap.cpp

---
+++ MPI/cas.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include <mpi.h>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    MPI_Init(&argc, &argv);
+
+    int rank;
+    int size;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    MPI_Win win;
+
+    int *window_buffer;
+    MPI_Alloc_mem(sizeof(int), MPI_INFO_NULL, &window_buffer);
+    window_buffer[0] = 0;
+
+    MPI_Win_create(window_buffer, sizeof(int), sizeof(int), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
+
+    MPI_Win_fence(0, win);
+
+    int val = 2;
+    int tmp;
+    MPI_Compare_and_swap(&val, window_buffer,&tmp, MPI_INT, 0, 0, win);
+
+    MPI_Win_fence(0, win);
+
+    string message = to_string(rank) + " " + to_string(window_buffer[0]) + " " + to_string(tmp) + "\n";
+    cout << message;
+
+    MPI_Win_free(&win);
+    MPI_Free_mem(window_buffer);
+    MPI_Finalize();
+
+    return 0;
+}
