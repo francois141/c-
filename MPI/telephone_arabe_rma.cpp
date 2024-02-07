f11e11cf2610f31ea4de3201968f30f529a4723b
François Costa <frankost.costa@gmail.com>
2024-02-07 15:39:03 +0100

    Add telephone_arabe_rma.cpp

---
+++ MPI/telephone_arabe_rma.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include <mpi.h>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+
+    MPI_Init(&argc, &argv);
+
+    int *a;
+
+    int output;
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    MPI_Alloc_mem(sizeof(int) * 100, MPI_INFO_NULL, &a);
+
+    if (rank == 0) {
+        a[0] = 1234567;
+    }
+
+    MPI_Win win;
+
+    MPI_Win_create(a, 100 * sizeof(int), sizeof(int), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
+
+    for (int i = 1; i < size; i++) {
+        MPI_Win_fence(0, win);
+
+        if (rank == i) MPI_Get(&output, 1, MPI_INT, i - 1, 0, 1, MPI_INT, win);
+
+        MPI_Win_fence(0, win);
+
+        if (rank == i) MPI_Put(&output, 1, MPI_INT, i, 0, 1, MPI_INT, win);
+
+        MPI_Win_fence(0, win);
+    }
+
+
+    cout << (to_string(a[0]) + "\n");
+
+    MPI_Win_free(&win);
+
+    MPI_Free_mem(a);
+
+    MPI_Finalize();
+
+
+    return 0;
+}
